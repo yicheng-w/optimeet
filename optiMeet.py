9a478d7530cb8fdf22d1f7d9903d9c9949dc1568
Yicheng Wang <alex.wyc2098@protonmail.ch>
2016-10-30 04:19:01 -0400

    updated

--- optiMeet.py
+++ optiMeet.py
@@ -1,22 +1,26 @@
 from flask import *
+import utils
+from database import DBManager
 
 app = Flask(__name__)
+database = DBManager()
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('create-event')
 def create_event():
-	return render_template('createEvent.html')
+    return render_template('createEvent.html')
 
 @app.route('join-event/<int:id>')
 def join_event(id):
-	return render_template('joinEvent.html', id=id)
+    return render_template('joinEvent.html', id=id)
 
 @app.route('view-event/<int:id>/<auth>/<name>')
 def view_event(id, auth, name):
-	return render_template('viewEvent.html', id=id, auth=auth, name=name)
+
+    return render_template('viewEvent.html', id=id, auth=auth, name=name)
 
 if (__name__ == '__main__'):
-	app.run(debug=True)
+    app.run(debug=True)
